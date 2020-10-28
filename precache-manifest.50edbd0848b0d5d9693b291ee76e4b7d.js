febcaa13554b4df51988c1f88793d9510d339e67
Abhishek koushal <akaushal451@gmail.com>
2020-10-28 12:36:26 +0530

    Updates

---
+++ precache-manifest.50edbd0848b0d5d9693b291ee76e4b7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03578f255982780fcf15db665dfcf5f6",
+    "url": "/ReactApp/index.html"
+  },
+  {
+    "revision": "0451a0413b84d0711da0",
+    "url": "/ReactApp/static/css/main.a9704b4c.chunk.css"
+  },
+  {
+    "revision": "70ba84b13850ef2c16b7",
+    "url": "/ReactApp/static/js/2.c62e5110.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactApp/static/js/2.c62e5110.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0451a0413b84d0711da0",
+    "url": "/ReactApp/static/js/main.7eba6043.chunk.js"
+  },
+  {
+    "revision": "c423c48d5156a497c4a5",
+    "url": "/ReactApp/static/js/runtime-main.54f26179.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
